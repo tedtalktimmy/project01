742e3b34a948050446e4a0b9aefd197f8c9e9513
tedtalktimmy <frtntn007@gmail.com>
2022-09-02 06:59:59 -0400

    added weather api to home page

--- assets/js/weather.js
+++ assets/js/weather.js
@@ -1,20 +1,34 @@
-const searchForm = document.querySelector('.search');
-const input = document.querySelector('.input');
-const newsList = document.querySelector('.news-list');
+let weather = {
+    "apiKey": "dec3546334c52626178cfd02c3288c55",
+    fetchWeather: function(city) {
+        fetch(
+            "https://api.openweathermap.org/data/2.5/weather?q="
+        + city
+        + "&units=imperial&appid="
+        + this.apiKey
+        )
+        .then((response) => response.json())
+        .then((data) => this.displayWeather(data));
+    },
+    displayWeather: function(data) {
+        const { name } = (data);
+        const { icon, description } = data.weather[0];
+        const { temp, humidity } = data.main;
+        const { speed } = data.wind;
+        console.log(name,icon,description,temp,humidity,speed);
+        document.querySelector(".city").innerText = "Weather in " + name;
+        document.querySelector(".icon").src = "https://openweathermap.org/img/wn/" + icon + ".png";
+        document.querySelector(".description").innerText = description;
+        document.querySelector(".temp").innerText = temp + "°F";
+        document.querySelector(".humidity").innerText = "humidity: " + humidity + "%";
+        document.querySelector(".wind").innerText = "wind speed: " + speed + "mph";
+    },
+    search: function () {
+        this.fetchWeather(document.querySelector(".search-bar").value);
+    }
+};
 
-searchForm.addEventListener('submit', retrieve)
+document.querySelector(".weather-btn").addEventListener("click", function() {
+weather.search();
+});
 
-function retrieve(e){
-
-    e.preventDefault()
-
-    const apiKey = 'XrNCA4bC_gdOUgg1hnN6d5ddc_N1MKTrOIFGm_kRlI0';
-    let topic = input.value;
-
-    let url =  `https://api.newscatcherapi.com/v2/search?q=${topic}&apiKey=${apiKey}`;
-
-    fetch(url).then((response) =>{
-        return response.json()
-    }).then((data)=>{
-        console.log(data);
-    })
