e5c6a56bd4b6c30be44531f79c4ff01063533020
Alex Jenkins <alexjenkins@Alexs-MacBook-Pro.local>
2022-09-02 10:43:42 -0400

    clean up session

---
+++ README.md
@@ -0,0 +1,23 @@
+# Project 1 
+
+# Description
+When you are working on trying to solve a problem as a web developer or building an application it can get overwhelming.
+This is the time when a mental break is needed most to let 
+Your mind settles. We have designed a webpage where you 
+can come breathe and recollect your thought process.
+LoafLounge is a placed designed for web developers too 
+come and ease their minds, comment on their day, and also
+a place for resources to hopefully solve their problem.
+So come on and loaf around. 
+
+# Visuals 
+
+![The Password Generator application displays a red button to "Generate Password".](/project01/readmain.png)
+
+![The Password Generator application displays a red button to "Generate Password".](/project01/readme2.png)
+
+# Live Links
+
+[Github Link](https://github.com/tedtalktimmy/project01)
+
+[Live Link](https://github.com/ajenkinsynwa/password-generator-/tree/master)
---
+++ assets/js/lofiradio.js
@@ -0,0 +1,73 @@
+
+// 2. This code loads the IFrame Player API code asynchronously.
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// 3. This function creates an <iframe> (and YouTube player)
+//    after the API code downloads.
+var player;
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('player', {
+    height: '250',
+    width: '450',
+    videoId: 'jfKfPfyJRdk',
+    static: "someValue",
+    playerVars: {
+      'playsinline': 1
+    },
+    events: {
+      'onReady': onPlayerReady,
+      'onStateChange': onPlayerStateChange
+    }
+  });
+}
+
+// 4. The API will call this function when the video player is ready.
+function onPlayerReady(event) {
+  event.target.playVideo();
+}
+
+// 5. The API calls this function when the player's state changes.
+//    The function indicates that when playing a video (state=1),
+//    the player should play for six seconds and then stop.
+var done = false;
+function onPlayerStateChange(event) {
+  if (event.data == YT.PlayerState.PLAYING && !done) {
+    
+    done = true;
+  }
+}
+function stopVideo() {
+  player.stopVideo();
+}
+
+
+
+$(document).ready(function(){ 
+ 
+  $(".primaryContained").on('click', function(){
+  $(".comment").addClass("commentClicked");
+});//end click
+$("textarea").on('keyup.enter', function(){
+  $(".comment").addClass("commentClicked");
+});//end keyup
+});//End Function
+
+new Vue({
+  el: "#app",
+  data:{
+     title: 'Add a comment',
+    newItem: '',
+    item: [],
+  },
+  methods:{
+    addItem  (){
+    this.item.push(this.newItem);
+      this.newItem = "";
+    }
+}
+
+});
